0c9cd337758d1bf0eef451ed1d7929fd5a307b43
Akobir <akobirshoimov1002@gmail.com>
2023-09-21 01:25:59 +0500

    changed project2 because i did not create urls

--- polls/urls.py
+++ polls/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from .views import CreateNewsView,UpdateNewsView,ListNewsView
 
 urlpatterns = [
-    path()
+    path('all/',ListNewsView.as_view()),
+    path('create/<int:news_id>/',CreateNewsView.as_view()),
+    path('update/',UpdateNewsView.as_view())
 ]
